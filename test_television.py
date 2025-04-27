a4d546ee891d5912816a08e91f87430ef7253698
sdjing <115203577+sdjing@users.noreply.github.com>
2025-04-27 15:27:58 -0500

    Update test_television.py
    Used Pytest and fixed code

--- test_television.py
+++ test_television.py
@@ -1,54 +1,60 @@
+import pytest
 from television import Television
 
 def test_init():
     tv = Television()
-    assert tv._Television__status == False
-    assert tv._Television__muted == False
-    assert tv._Television__volume == tv.MIN_VOLUME
-    assert tv._Television__channel == tv.MIN_CHANNEL
+    assert str(tv) == "Power = Off, Channel = 0, Volume = 0"
 
-def test_power():
+def test_power_on_off():
     tv = Television()
     tv.power()
-    assert tv._Television__status == True
+    assert str(tv).startswith("Power = On")
     tv.power()
-    assert tv._Television__status == False
+    assert str(tv).startswith("Power = Off")
 
-def test_mute():
+def test_mute_unmute():
     tv = Television()
+    tv.power()
+    tv.volume_up()
     tv.mute()
-    assert tv._Television__muted == True
+    assert "Volume" in str(tv)
     tv.mute()
-    assert tv._Television__muted == False
+    assert "Volume" in str(tv)
 
 def test_channel_up():
     tv = Television()
     tv.power()
     tv.channel_up()
-    assert tv._Television__channel == tv.MIN_CHANNEL + 1
+    assert "Channel = 1" in str(tv)
     tv.channel_up()
-    assert tv._Television__channel == tv.MIN_CHANNEL + 2
+    tv.channel_up()
+    tv.channel_up()
+    assert "Channel = 0" in str(tv)  
 
 def test_channel_down():
     tv = Television()
     tv.power()
     tv.channel_down()
-    assert tv._Television__channel == tv.MIN_CHANNEL - 1
+    assert "Channel = 3" in str(tv) 
     tv.channel_down()
-    assert tv._Television__channel == tv.MIN_CHANNEL - 2
-    
+    assert "Channel = 2" in str(tv)
+
 def test_volume_up():
     tv = Television()
     tv.power()
     tv.volume_up()
-    assert tv._Television__volume == tv.MIN_VOLUME + 1
+    assert "Volume = 1" in str(tv)
     tv.volume_up()
-    assert tv._Television__volume == tv.MIN_VOLUME + 2
+    tv.volume_up()
+    assert "Volume = 2" in str(tv)  
 
 def test_volume_down():
     tv = Television()
     tv.power()
+    tv.volume_up()
+    tv.volume_up()
     tv.volume_down()
-    assert tv._Television__volume == tv.MIN_VOLUME - 1
+    assert "Volume = 1" in str(tv)
     tv.volume_down()
-    assert tv._Television__volume == tv.MIN_VOLUME - 2
+    tv.volume_down()
+    assert "Volume = 0" in str(tv)  
